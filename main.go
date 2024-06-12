a337b47a8bf087336a181f2417b888be9cbf9c71
Marcelo de Andrade Silva <marcelo.andrade@grupomulti.com.br>
2024-06-12 11:27:38 -0300

    feat: Init gin and run api with /ping route

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//Inicializa o router utilizando a configurações Default do Gin
+	r := gin.Default()
+
+	//Definindo uma Rota
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	//Roda a Api
+	r.Run()
+}
